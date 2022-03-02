1025c4021d5556561b800f02fb785af042a61092
Yonatan Haddish <yonihailshn@gmail.com>
2022-03-02 17:23:32 -0500

    tutorial

--- test.hs
+++ test.hs
@@ -118,3 +118,15 @@ joinOneMoreWord = joinWords "hello" "world"
 largestDivisible :: (Integral a) => a
 largestDivisible  = head (filter p [100000,99999..])
     where p x = x `mod` 3829 == 0
+
+sumA = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n 
+    | even n = n:chain (n `div` 2)
+    | odd n = n:chain ((n*3) + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
