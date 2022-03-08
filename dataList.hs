55647469070873b449388941075b8f80d4201ab7
Yonatan Haddish <yonihailshn@gmail.com>
2022-03-08 12:29:47 -0500

    tutorial

--- dataList.hs
+++ dataList.hs
@@ -1,3 +1,8 @@
 import Data.List
 
-interSperse = intersperse '.' "Monkey"
+interSperseA = intersperse '.' "Monkey"
+interSperseB = intersperse 9 [1,2,3,4,5,6,7,8]
+
+sumPoly = map sum $ transpose [[0,3,5,9], [10,0,0,9], [8,5,1,-1]]
+
+concatList = concat [[1,2], [3,4], [5,6]]
