dbe3b86732042a0bf0730d67b001952e0c3939f3
Yonatan Haddish <yonihailshn@gmail.com>
2022-03-16 13:09:06 -0400

    plutus-tutorial

--- plutus-tutorial.hs
+++ plutus-tutorial.hs
@@ -1,3 +1,6 @@
  {-# INLINABLE mkValidator #-}
 mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkValidator _ _ _ = ()
+
+validator :: Validator
+validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
