58a9565eb5aefe804b5858f2dd211fa8987a00d3
Yonatan Haddish <yonihailshn@gmail.com>
2022-03-29 16:53:21 -0400

    plutus tutorial

--- test.hs
+++ test.hs
@@ -151,6 +151,10 @@ minLovelace = 2000000
 myFirstFunction :: String -> String
 myFirstFunction input = "Hello " ++ input ++ "!"
 
-auctionValidator :: Validator
-auctionValidator = Scripts.validatorScript typedAuctionValidator
-
+nameDo :: IO ()
+nameDo = do putStr "What is your first name? "
+            first <- getLine
+            putStr "And your last name? "
+            last <- getLine
+            let full = first ++ " " ++ last
+            putStrLn ("Pleased to meet you, " ++ full ++ "!")
