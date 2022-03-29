c245e2a4595dbd79fbd71febb0de231a3ce67c95
Yonatan Haddish <yonihailshn@gmail.com>
2022-03-29 10:08:44 -0400

    plutus tutorial

--- plutus-tutorial.hs
+++ plutus-tutorial.hs
@@ -77,3 +77,30 @@ mkSchemaDefinitions ''GiftSchema
 mkKnownCurrencies []
 
 
+________________________________________________________
+
+import Data.Text qualified as T
+import Playground.Contract
+import Plutus.Contract
+import PlutusTx.Prelude
+import Prelude qualified as Haskell
+
+-- | A 'Contract' that logs a message.
+hello :: Contract () EmptySchema T.Text ()
+hello = logInfo @Haskell.String "Love The Life You Live, Live The Life You Love"
+
+endpoints :: Contract () EmptySchema T.Text ()
+endpoints = hello
+
+-- 'mkSchemaDefinitions' doesn't work with 'EmptySchema'
+-- (that is, with 0 endpoints) so we define a
+-- dummy schema type with 1 endpoint to make it compile.
+-- TODO: Repair 'mkSchemaDefinitions'
+type DummySchema = Endpoint "Click Me" ()
+
+mkSchemaDefinitions ''DummySchema
+
+$(mkKnownCurrencies [])
+
+
+
