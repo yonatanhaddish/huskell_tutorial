5f005d6b5ff8de5052762ebfc7a9f63728d565ee
Yonatan Haddish <yonihailshn@gmail.com>
2022-04-06 15:06:25 -0400

    tutorial

---
+++ Gift.hs
@@ -0,0 +1,25 @@
+import Plutus.Contract
+import PlutusTx (Data (..))
+import qualified PlutusTx
+import qualified PlutusTx.Builtins as Builtins
+import PlutusTx.Prelude hiding (Semigroup(..), unless)
+import Ledger hiding (singleton)
+import Ledger.Constraints as Constraints
+import qualified Ledger.Scripts as Scripts
+import Ledger.Ada as Ada
+import Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage)
+import Playground.TH (mkKnownCurrencies, mkSchemaDefinitions)
+import Playground.Types (KnownCurrency (..))
+import Prelude (IO, Semigroup (..), String)
+import Text.Printf (printf)
+
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+{-# INLINABLE mkValidator #-}
+mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkValidator _ _ _ = ()
+
+validator :: Validator
+validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
+
+
