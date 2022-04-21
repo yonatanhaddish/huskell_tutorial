aeb5f59adf713de38fe6c5a2c6cde394c730d52e
Yonatan Haddish <yonihailshn@gmail.com>
2022-04-21 14:29:17 -0400

    tutorial on maybe

--- week04/monads.hs
+++ week04/monads.hs
@@ -1,15 +1,28 @@
-main :: IO ()
-main = putStrLn "Hello World!"
 
-nameDo :: IO ()
-nameDo = do putStr "What is your first name? "
-            first <- getLine
-            putStr "And your last name? "
-            last <- getLine
-            let full = first ++ " " ++ last
-            putStrLn ("Pleased to meet you, " ++ full ++ "!")
+import Text.Read ( readMaybe )
 
-bar :: IO ()
-bar = getLine >>= \s -> 
-      getLine >>= \t -> 
-      putStrLn ("Output: " ++ s ++ " " ++ t)
+foo :: String -> String -> String -> Maybe Int
+foo x y z = case readMaybe x of
+    Nothing -> Nothing
+    Just k  -> case readMaybe y of
+        Nothing -> Nothing
+        Just l  -> case readMaybe z of
+            Nothing -> Nothing
+            Just m  -> Just (k + l + m)
+
+-- main :: IO ()
+-- main = putStrLn "Hello World!"
+
+-- nameDo :: IO ()
+-- nameDo = do putStr "What is your first name? "
+--             first <- getLine
+--             putStr "And your last name? "
+--             last <- getLine
+--             let full = first ++ " " ++ last
+--             putStrLn ("Pleased to meet you, " ++ full ++ "!")
+
+-- bar :: IO ()
+-- bar = getLine >>= \s -> 
+--       getLine >>= \t -> 
+--       putStrLn ("Output: " ++ s ++ " " ++ t)
+
