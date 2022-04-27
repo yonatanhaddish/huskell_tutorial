8a42be4439cdce5e6f07701ba44e942fd09e3d92
Yonatan Haddish <yonihailshn@gmail.com>
2022-04-27 11:15:43 -0400

    week05 plutus tutorial

---
+++ week05/value.hs
@@ -0,0 +1,13 @@
+singleton :: CurrentySymbol -> TokenName -> Integer -> Value
+singleton "a8ff" "ABC" 7 
+Value (map [(a8ff, map [("ABC", 7)])])
+
+    map [(a8ff, map [("ABC", 7)])]
+
+
+
+
+
+
+
+value (map [( , map [("", 42)]), (a8ff, map [("ABC", 7),("XYZ", 100)])])
