14bd08d6220530b81527197029476234fb8be33f
Yonatan Haddish <yonihailshn@gmail.com>
2022-05-06 13:04:16 -0400

    haskell tutotial

--- Haskell/day01.hs
+++ Haskell/day01.hs
@@ -1,5 +1,15 @@
+-- Quick Sort 
 qSort [] = []
 qSort (x:xs)  = qSort ys ++ [x] ++ qSort zs
                 where
                     ys = [a | a <- xs, a <= x]
                     zs = [b | b <- xs, b > x]
+
+heaD [] = []
+heaD (x:xs) = [x]
+
+doubleX :: Int -> Int
+doubleX x = x + x
+
+quadrapleY :: Int -> Int
+quadrapleY y = doubleX (doubleX y)
