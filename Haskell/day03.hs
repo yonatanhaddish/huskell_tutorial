c0b620c0e88e14ea9dc74f23990096bbdc6903ff
Yonatan Haddish <yonihailshn@gmail.com>
2022-05-11 11:59:00 -0400

    haskell tutorial on defining finctions

--- Haskell/day03.hs
+++ Haskell/day03.hs
@@ -5,13 +5,32 @@
 abS :: Int -> Int
 abS n = if n >= 0 then n else -n
 
+------------------------------------------------------------------------------------
 isCarni :: String -> String
 isCarni n = if n == "lion" then "Carni" else
     if n == "sheep" then "Herbi" else "Omni"
 
+-- guarded condition
+isCarni' :: String -> String
+isCarni' n | n == "lion" = "Carni"
+           | n == "sheep" = "Herbi"
+           | otherwise    = "Omni"
+------------------------------------------------------------------------------------
 printNum :: Int -> String
 printNum n = if n == 1 then "One" else 
     if n == 2 then "Two" else 
         if n == 3 then "Three" else
             if n == 4 then "Four" else 
                 if n == 5 then "Five" else "Number Not Found on Database"
+
+printNum' :: Int -> String
+printNum' n | n == 1 = "One"
+            | n == 2 = "Two"
+            | n == 3 = "Three"
+            | n == 4 = "Four"
+            | n == 5 = "Five"
+            | otherwise = "Number not found on database"
+------------------------------------------------------------------------------------
+
+-- Lambda Expressions
+
