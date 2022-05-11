89e75057d489fb004af87a8305ac2edf6cf82450
Yonatan Haddish <yonihailshn@gmail.com>
2022-05-11 10:24:24 -0400

    haskell tutorial on defining finctions

--- Haskell/day03.hs
+++ Haskell/day03.hs
@@ -1,6 +1,17 @@
 -- Defining Functions
 
 
--- Absoulute value
+-- if else condition
 abS :: Int -> Int
 abS n = if n >= 0 then n else -n
+
+isCarni :: String -> String
+isCarni n = if n == "lion" then "Carni" else
+    if n == "sheep" then "Herbi" else "Omni"
+
+printNum :: Int -> String
+printNum n = if n == 1 then "One" else 
+    if n == 2 then "Two" else 
+        if n == 3 then "Three" else
+            if n == 4 then "Four" else 
+                if n == 5 then "Five" else "Number Not Found on Database"
